5b5856f5ea2287f81adf7c51be6fa2f388712465
Saliya <saliyas@mobitel.lk>
2018-10-29 14:40:00 +0530

    rest services

--- src/main/java/com/fare/compare/fare/rest/FareRestService.java
+++ src/main/java/com/fare/compare/fare/rest/FareRestService.java
@@ -1,10 +1,15 @@
 package com.fare.compare.fare.rest;
 
 import com.fare.compare.fare.model.Fare;
+import com.fare.compare.fare.model.ProviderPrice;
+import com.fare.compare.fare.service.FareComparator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * User: Saliya Samarawickrama
@@ -15,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/fare")
 public class FareRestService {
 
+    @Autowired
+    FareComparator comparator;
     @GetMapping("/getfare")
-    Fare getFare(@RequestParam(name = "start") String start, @RequestParam(name = "end") String end){
-        return new Fare();
+    ProviderPrice getFare(@RequestParam(name = "start") String start, @RequestParam(name = "end") String end){
+        List<ProviderPrice> prices = null;
+        return comparator.comparePrices(prices);
     }
 
 }
