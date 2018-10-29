42f418a8d82847d86a8b5c61267081cacae22be6
nalaka <nalakap@mobitel.lk>
2018-10-29 15:05:56 +0530

    rest client initiated

--- src/main/java/com/fare/compare/fare/external/ExternalFareRateClient.java
+++ src/main/java/com/fare/compare/fare/external/ExternalFareRateClient.java
@@ -1,21 +1,22 @@
 package com.fare.compare.fare.external;
 
+import com.fare.compare.fare.model.ProviderPrice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Author  : nalaka 
  * Project : fare 
  * Date    : 10/29/18
  */
-@Service
-public class ExternalFareRateClient {
+public interface ExternalFareRateClient {
 
 
-
-
+    ProviderPrice[] getProviderPriceList(String start,String end);
 
 }
 
--- src/main/java/com/fare/compare/fare/external/impl/ExternalFareRateClientImpl.java
+++ src/main/java/com/fare/compare/fare/external/impl/ExternalFareRateClientImpl.java
@@ -1,8 +1,15 @@
 package com.fare.compare.fare.external.impl;
 
+import com.fare.compare.fare.external.ExternalFareRateClient;
+import com.fare.compare.fare.model.ProviderPrice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Author  : nalaka 
@@ -10,11 +17,19 @@ import org.springframework.web.client.RestTemplate;
  * Date    : 10/29/18
  */
 @Component
-public class ExternalFareRateClientImpl {
+public class ExternalFareRateClientImpl implements ExternalFareRateClient{
 
     @Autowired
     RestTemplate template;
 
 
+    @Override
+    public ProviderPrice[] getProviderPriceList(String start, String end) {
+        Map<String,String> params=new HashMap<>();
+        params.put("","");
+        params.put("","");
+        //ResponseEntity<Object[]> entity= template.getForEntity("",ProviderPrice.class,params);
+    return null;
+    }
 }
 
